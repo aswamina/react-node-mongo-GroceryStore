5202164e9c1c83e5cc42ed9158fe6e2e1a341a83
Anand Swaminathan <aswamina@gmail.com>
2016-11-07 17:32:37 -0600

    changes for body-parser and mongoose

--- server/main.js
+++ server/main.js
@@ -2,9 +2,17 @@ var express = require('express');
 
 var app = new express();
 
+var parser = require('body-parser');
+require('./database.js');
+
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/../.tmp'));
 app.get('/', function (request, response) {
     response.render('./../app/index', {});
 
 }).listen(5001);
+app.use(parser.json());
+app.use(parser.urlencoded({
+    extended: false
+}));
+require('./routes/items.js')(app);
